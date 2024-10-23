7d05eb36c3f4d61ce63d9ef08a66de421d1add83
GuangYu-yu <77334277+GuangYu-yu@users.noreply.github.com>
2024-10-23 14:50:02 +0800

    Update query_ip.py

--- query_ip.py
+++ query_ip.py
@@ -93,6 +93,11 @@ async def query_dns_uk_lon(session, domain):
         f"https://uk-lon.doh.sb/dns-query?name={domain}&type=A",
         f"https://uk-lon.doh.sb/dns-query?name={domain}&type=AAAA")
 
+async def query_dns_de_fra(session, domain):
+    return await query_dns_json(session, 
+        f"https://de-fra.doh.sb/dns-query?name={domain}&type=A",
+        f"https://de-fra.doh.sb/dns-query?name={domain}&type=AAAA")
+
 async def process_domains(domains, query_func, semaphore):
     results = []
     async with aiohttp.ClientSession() as session:
@@ -111,8 +116,8 @@ async def main(query_method):
         all_domains = f.read().splitlines()
 
     # 新的查询方法和比例
-    query_methods = ['bgp', 'google', 'quad9', 'twnic', 'uk_lon', 'sb', 'kr_sel', 'sg_sin', 'jp_nrt', 'hk_hkg']
-    method_ratios = {'bgp': 1, 'google': 1, 'quad9': 1, 'twnic': 1, 'uk_lon': 1, 'sb': 1, 'kr_sel': 1, 'sg_sin': 1, 'jp_nrt': 1, 'hk_hkg': 1}
+    query_methods = ['de_fra', 'google', 'quad9', 'twnic', 'uk_lon', 'sb', 'kr_sel', 'sg_sin', 'jp_nrt', 'hk_hkg']
+    method_ratios = {'de_fra': 58, 'google': 71, 'quad9': 71, 'twnic': 58, 'uk_lon': 62, 'sb': 67, 'kr_sel': 62, 'sg_sin': 40, 'jp_nrt': 51, 'hk_hkg': 45}
     total_ratio = sum(method_ratios.values())
 
     total_domains = len(all_domains)
@@ -134,7 +139,7 @@ async def main(query_method):
     semaphore = asyncio.Semaphore(10)  # 限制并发查询数为10
 
     query_functions = {
-        'bgp': query_bgp,
+        'de_fra': query_dns_de_fra,
         'google': query_dns_google,
         'quad9': query_dns_quad9,
         'twnic': query_dns_twnic,
